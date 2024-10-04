e31dec430d6f4f2c1ff02c4b5ce9a86f70ce68ad
mmdreza zeydani <mmdzyzfrontendz2002@gmail.com>
2024-10-04 21:02:30 +0330

    wrote data for my header in headerData file

---
+++ src/components/header/headerData.ts
@@ -0,0 +1,214 @@
+import xboxLogo from "../../assets/xboxLogo.png"
+import microsoftLogo from "../../assets/MicrosoftLogo.png"
+import headerPic from "../../assets/headerPic.jpg"
+export const NavBtnsData = [
+  {
+    title: "Game Pass",
+    btns: [
+      "Join Game Pass",
+      "Browse Games",
+      "Game Pass Ultimate",
+      "PC Game Pass",
+      "Game Pass Core",
+      "Deals with Game Pass",
+      "Xbox Cloud Gaming",
+    ],
+  },
+  {
+    title: "Games",
+    btns: [
+      "Games",
+      "Games home",
+      "Shop all PC games",
+      "Cloud games",
+      "Free-to-Play games",
+      "Backward compatible games",
+      "Sales & Specials",
+      "Redeem Code",
+    ],
+  },
+  {
+    title: "Devices",
+    btns: [
+      {
+        btnsTitle: "Cosoles",
+        titleBtns: [
+          "Xbox consoles",
+          "Xbox Series X",
+          "Xbox Series S",
+          "Xbox All Access",
+          "Shop all consoles",
+          "Help Me Choose",
+          "Where to buy",
+        ],
+      },
+      {
+        btnsTitle: "Accessories",
+        titleBtns: [
+          "Shop all accessories,",
+          "Design your controller",
+          "Controller",
+          "Headsets",
+          "Hard drives & storage",
+          "Mobile gaming accessories",
+        ],
+      },
+      {
+        btnsTitle: "PC Devices",
+        titleBtns: ["Gaming PCs"],
+      },
+    ],
+  },
+  {
+    title: "Play",
+    btns: [""],
+  },
+  {
+    title: "Store",
+    btns: [
+      "Shop Games",
+      "Shop Game Pass",
+      "Shop Console",
+      "Shop Accessories",
+      "Shop Deals",
+    ],
+  },
+  {
+    title: "Support",
+    btns: [
+      {
+        btnsTitle: "Support home",
+        titleBtns: [""],
+      },
+      {
+        btnsTitle: "Xbox status",
+        titleBtns: [""],
+      },
+      {
+        btnsTitle: "Help topics",
+        titleBtns: [
+          "Account & profile",
+          "Subscriptions & billing",
+          "Hardware & networking",
+          "Family & online safety",
+          "Games & apps",
+          "Friends & social activity",
+        ],
+      },
+      {
+        btnsTitle: "Accessible gaming",
+        titleBtns: [""],
+      },
+      {
+        btnsTitle: "Xbox system updates",
+        titleBtns: [""],
+      },
+    ],
+  },
+  {
+    title: "More",
+    btns: [
+      {
+        btnsTitle: "My Xbox",
+        titleBtns: ["Profile", "Rewards", "Xbox Mastercard", "Wish list"],
+      },
+      {
+        btnsTitle: "Developers",
+        titleBtns: ["Games", "Designed for Xbox"],
+      },
+    ],
+  },
+];
+export const AllMicrosoft = [
+  {
+    mainTitle: "All Microsoft",
+    titles: [
+      "Microsoft 365",
+      "Teams",
+      "Windows",
+      "Surface",
+      "Xbox",
+      "Deals",
+      "Small Business",
+      "Support",
+    ],
+    btns: [
+      {
+        btnsTitle: "Software",
+        titleBtns: [
+          "Windows Apps",
+          "AI",
+          "Outlook",
+          "OneDrive",
+          "Microsoft Teams",
+          "OneNote",
+          "Microsoft Edge",
+          "Skype",
+        ],
+      },
+      {
+        btnsTitle: "Pcs & Devices",
+        titleBtns: [
+          "Computers",
+          "Shop Xbox",
+          "Accessories",
+          "VR & mixed reality",
+          "Certified Refurbished",
+          "Trade-in for cash",
+        ],
+      },
+      {
+        btnsTitle: "Entertainment",
+        titleBtns: [
+          "Xbox Game Pass Ultimate",
+          "PC Game Pass",
+          "Xbox games",
+          "PC and Windows games",
+          "Movies & TV",
+        ],
+      },
+      {
+        btnsTitle: "Business",
+        titleBtns: [
+          "Microsoft Cloud",
+          "Microsoft Security",
+          "Dynamics 365",
+          "Microsoft 365 for business",
+          "Microsoft Power Platform",
+          "Windows 365",
+          "Microsoft Industry",
+          "Small Busines",
+        ],
+      },
+      {
+        btnsTitle: "Developer & IT",
+        titleBtns: [
+          "Azure",
+          "Developer Center",
+          "Documentation",
+          "Microsoft Learn",
+          "Microsoft Tech Community",
+          "Azure Marketplace",
+          "AppSource",
+          "Visual Studio",
+        ],
+      },
+      {
+        btnsTitle: "Other",
+        titleBtns: [
+          "Microsoft Rewards",
+          "Free downloads & security",
+          "Education",
+          "Gift cards",
+          "Licensing",
+          "Unlocked stories",
+        ],
+      },
+    ],
+  },
+];
+export const Images={
+    xboxLogo,
+    microsoftLogo,
+    headerPic
+}
